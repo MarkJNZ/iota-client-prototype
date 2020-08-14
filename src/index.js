d014744b06ef31b54a37d5da42afdd286c158fd6
MarkJ <45180004+MarkJNZ@users.noreply.github.com>
2020-08-14 17:50:38 +1200

    added bp iota transaction

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-var http = require("http");
+// var http = require("http");
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+// //create a server object:
+// http
+//   .createServer(function(req, res) {
+//     res.write("Hello World!"); //write a response to the client
+//     res.end(); //end the response
+//   })
+//   .listen(8080); //the server object listens on port 8080
+
+// Require the client library packages
+const Iota = require("@iota/core");
+
+// Create a new instance of the IOTA API object
+// Use the `provider` field to specify which node to connect to
+const iota = Iota.composeAPI({
+  provider: "https://nodes.devnet.iota.org:443"
+});
+
+const depth = 3;
+const minimumWeightMagnitude = 9;
+
+const address =
+  "HEQLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWOR99D";
+
+const seed =
+  "PUEOTSEITFEVEWCWBTSIZM9NKRGJEIMXTULBACGFRQK9IMGICLBKW9TTEVSDQMGWKBXPVCBMMCXWMNPDX";
+
+const message = JSON.stringify({ message: "Hello world" });
+const messageInTrytes = Converter.asciiToTrytes(message); //a tryte is just a character between A-Z or the number
